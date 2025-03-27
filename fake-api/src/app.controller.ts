9ff002d2a5835c0be9c73616d029e2ed1bf78302
DawLip <d.lipinski022@gmail.com>
2025-03-27 06:44:45 +0100

    add GET /dashboard and GET /aside endpoints

--- fake-api/src/app.controller.ts
+++ fake-api/src/app.controller.ts
@@ -6,7 +6,11 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  getHello(): string { return this.appService.getHello(); }
+
+  @Get('dashboard')
+  getDashboard(): any { return this.appService.getDashboard();}
+
+  @Get('aside')
+  getAside(): any { return this.appService.getAside(); }
 }
--- fake-api/src/app.service.ts
+++ fake-api/src/app.service.ts
@@ -5,4 +5,61 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  getDashboard(): any {
+    const invitation = {
+      id: 0,
+      name: "John",
+      surname: "Doe",
+      isActive: true
+    }
+    const message = {
+      messageAuthor: "you", 
+      message: "hey :D"
+   }
+    const lastChat = {
+      userId: 0,
+      name: "John",
+      surname: "Doe",
+      messages: [message,message,message],
+      isActive: true
+    }
+    const group = {
+      groupId: 0,
+      name: "Python lovers",
+      messages: [message,message,message],
+      isActive: true
+    }
+    const mayK = {
+      id: 0,
+      name: "John",
+      surname: "Doe",
+      isActive: true
+    }
+    return ({
+      invitations: [invitation,invitation],    
+      lastChats: [lastChat,lastChat,lastChat,lastChat,lastChat,lastChat],
+      groups: [group,group,group,group,group,group],
+      mayKnow: [mayK,mayK,mayK,mayK,mayK,mayK,mayK,mayK,mayK,mayK,mayK,mayK]
+    });
+  }
+  getAside(): any {
+    const friend = {
+      name: "John",
+      surname: "Doe",
+      lastMessage: "hey :D",
+      lastMessageAuthor: "you",
+      isActive: true
+    }
+    const group = {
+      name: "Python lovers",
+      lastMessage: "Hey :D",
+      lastMessageAuthor: "you",
+      isActive: true
+    }
+  
+    return ({
+      friends: [friend,friend,friend,friend,friend,friend],
+      groups: [group,group,group,group]
+    })
+  }
 }
